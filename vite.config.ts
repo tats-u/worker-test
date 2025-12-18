d53585be0c92f710c49f1ca6a26ba8bb461a7aca
Tatsunori Uchino <tats.u@live.jp>
2025-12-18 23:44:28 +0900

    Deploy GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: process.env.CHANGE_BASE ? '/worker-test/' : undefined,
   plugins: [solid()],
 })
